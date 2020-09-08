47e9909865ef0acf36101d7231084d1b3918beaa
HectorP55 <hectorp55@tamu.edu>
2020-09-08 16:44:20 -0500

    Shrinking Nav Bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  @ViewChild('NavBar') navBar;
+  public navHeight: string;
+  public navFontSize: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      this.navFontSize = '16px';
+      this.navHeight = '80px';
+    } else {
+      this.navFontSize = '20px';
+      this.navHeight = '140px';
+    }
+  }
 }
